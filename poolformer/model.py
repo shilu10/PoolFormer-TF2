db3d128ca47791c164569f8df6297648b0ffa8e7
adminuser <adminuser@kali.mvm>
2023-08-01 08:44:38 +0530

    added code for model weight porting for torch->tf

--- poolformer/model.py
+++ poolformer/model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf 
 import numpy as np 
-from .blocks import MetaFormerStage, Stem
-from .layers import norm_layer_factory, act_layer_factory
+from .blocks import MetaFormerStage 
+from .layers import norm_layer_factory, act_layer_factory, Stem
 from .layers import Pooling
 
 
